042b33fcd4da009f623ee89c6f47df363728de79
James Davis <32335838+JamesDavis18@users.noreply.github.com>
2020-10-15 13:41:07 +0100

    Add files via upload

---
+++ Game_pages/analytics.js
@@ -0,0 +1,7 @@
+
+window.dataLayer = window.datalayer || [];
+function gtag()[dataLayer.push(arguments);}
+gtag('js', new Date());
+	
+gtag('config', GA_TRACKING_ID')
+gtag('config', GA_TRACKING_ID', { 'send_page_view': true });
